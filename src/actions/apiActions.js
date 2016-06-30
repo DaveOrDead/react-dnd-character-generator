7c337c352168cc76cdd05c5b366f1091ee7dc739
DaveOrDead <davidajberner@gmail.com>
2016-06-30 08:00:16 +0100

    fix up reducer and actions

--- src/actions/apiActions.js
+++ src/actions/apiActions.js
@@ -1,15 +1,15 @@
 import {FETCH_DATA} from '../constants/actionTypes';
 import axios from "axios";
 
-const ROOT_URL = 'https://dnd-character-gen-server.herokuapp.com/api/alignments';
+const ROOT_URL = 'https://dnd-character-gen-server.herokuapp.com/api/';
 
 export function fetchData(data){
-    const url = ROOT_URL;
-    // console.log(url);
+    const url = ROOT_URL + data;
     const request = axios.get(url);
+
     return {
         type: FETCH_DATA,
-        payload: request
+        payload: request,
+        data
     };
-
 }
--- src/reducers/apiReducer.js
+++ src/reducers/apiReducer.js
@@ -1,13 +1,17 @@
 import {FETCH_DATA} from '../constants/actionTypes';
+import objectAssign from 'object-assign';
 
 export default function (state = [], action) {
+    let newState;
+
     switch(action.type){
 
-    case FETCH_DATA:
-        // console.log(action.payload)
-        return [...state, ...action.payload.data];
+        case FETCH_DATA:
+            newState = objectAssign({}, state);
+            newState[action.data] = action.payload.data;
+            return newState;
 
-    default :
-        return state;
-    }
+        default :
+            return state;
+        }
 }
