9ca161c7303d2b409ee75b95d020f5209657954f
DaveOrDead <davidajberner@gmail.com>
2016-07-02 12:46:45 +0100

    update ViewAbilitiesPage to use server data

--- src/containers/viewAbilitiesPage/ViewAbilitiesPage.js
+++ src/containers/viewAbilitiesPage/ViewAbilitiesPage.js
@@ -1,34 +1,71 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {abilities} from '../../data/abilities';
+import {bindActionCreators} from 'redux';
+
+import * as apiActions from '../../actions/apiActions';
 import Ability from '../ability';
 
-const ViewAbilityPage = ({character}) => {
+
+class ViewAbilitiesPage extends React.Component {
+
+    componentDidMount(){
+        const {actions} = this.props;
+        actions.fetchDataIfNeeded('abilities');
+    }
+
+    render() {
+
+        const {abilities = [], actions, character, isFetching = true} = this.props;
 
         return (
                 <div>
-                    {abilities.map(ability => {
-                        return (<Ability
-                           key={ability.id}
-                           abilityId={ability.id}
-                           text={ability.name}
-                           race={character.race}
-                       />);
-                    })}
-                </div>
+                    {isFetching && abilities.length === 0 &&
+                    <h2>Loading...</h2>
+                    }
+                    {!isFetching && abilities.length === 0 &&
+                    <h2>Empty.</h2>
+                    }
+                    {abilities.length > 0 &&
+                    <div>
+                        {abilities.map(ability => {
+                            return (<Ability
+                               key={ability.id}
+                               abilityId={ability.id}
+                               text={ability.name}
+                               race={character.race}
+                           />);
+                        })}
+                    </div>
+                }
+            </div>
             );
-    };
+        }
+    }
 
-ViewAbilityPage.propTypes = {
-    character: React.PropTypes.object.isRequired
+ViewAbilitiesPage.propTypes = {
+    abilities: React.PropTypes.array,
+    actions: React.PropTypes.object.isRequired,
+    character: React.PropTypes.object.isRequired,
+    isFetching: React.PropTypes.bool
 };
+
 
 function mapStateToProps(state) {
     return {
+        isFetching: state.api.isFetching,
+        abilities: state.api.abilities,
         character: state.character
     };
 }
 
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(apiActions, dispatch)
+    }
+};
+
+
 export default connect(
-    mapStateToProps
-)(ViewAbilityPage);
+    mapStateToProps,
+    mapDispatchToProps
+)(ViewAbilitiesPage);
