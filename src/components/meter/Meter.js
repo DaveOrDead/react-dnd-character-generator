2c6a80b9ed0d5c4076da9522a9fe2db541e528d1
DaveOrDead <davidajberner@gmail.com>
2016-07-03 09:45:13 +0100

    meter component

---
+++ src/components/meter/Meter.js
@@ -0,0 +1,40 @@
+import React, {PropTypes} from 'react';
+import getClassNameString from '../../utils/getClassNameString';
+
+
+const Meter = ({
+    isEmpty,
+    isActive,
+    max,
+    value
+}) => {
+
+    const barStyle = {
+        transform: max ? `scaleX(${value / max})` : ''
+    };
+console.log(barStyle);
+
+    const classList = getClassNameString({
+        'c-meter': true,
+        'is-active': isActive,
+        'is-empty': isEmpty,
+    });
+
+    return (
+        <div className={classList}>
+            <span className="c-meter__fill" style={barStyle} />
+        </div>
+    );
+};
+
+
+
+Meter.propTypes = {
+    isActive: PropTypes.bool,
+    isEmpty: PropTypes.bool,
+    max: PropTypes.integer,
+    value: PropTypes.integer
+};
+
+
+export default Meter;
