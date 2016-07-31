3d6cc5e176f9b23753e1953157258078ac56eb3a
DaveOrDead <davidajberner@gmail.com>
2016-07-31 16:16:42 +0100

    add feats

--- src/containers/featsPage/FeatsPage.js
+++ src/containers/featsPage/FeatsPage.js
@@ -1,15 +1,82 @@
 import React from 'react';
-import { Link } from 'react-router';
 
-const FeatsPage = () => {
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as apiActions from '../../actions/apiActions';
+
+
+class FeatsPage extends React.Component {
+
+    componentDidMount(){
+        const {actions} = this.props;
+        actions.fetchDataIfNeeded('feats');
+    }
+    render() {
+
+        const {feats = [], isFetching = true} = this.props;
+
 
         return (
             <div>
-               <h1>Complete</h1>
-               <Link to="/character/251">Save & View</Link>
+            {isFetching && feats.length === 0 &&
+                <h2>Loading...</h2>
+            }
+            {!isFetching && feats.length === 0 &&
+              <h2>Empty.</h2>
+            }
+            {feats.length > 0 &&
+            <div style={{ opacity: isFetching ? 0.5 : 1 }}>
+                <table className="c-table">
+                    <thead>
+                        <tr>
+                            <th>Id</th><th>Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                   {feats.map((item, i) =>
+
+                        <tr key={i}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                        </tr>
+
+                    )}
+                   </tbody>
+                   </table>
+
 
             </div>
+            }
+            </div>
             );
-    };
+        }
+    }
 
-export default FeatsPage;
+FeatsPage.propTypes = {
+    actions: React.PropTypes.object,
+    character: React.PropTypes.object,
+    feats: React.PropTypes.array,
+    isFetching: React.PropTypes.bool
+};
+
+
+function mapStateToProps(state) {
+  return {
+    feats: state.api.feats,
+    character: state.character,
+    isFetching: state.api.isFetching
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(apiActions, dispatch)
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(FeatsPage);
+
